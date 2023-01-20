18d11d35b62cd7518167886cbbd3b15274de4d4e
Jude odika <judeodika@gmail.com>
2023-01-20 04:49:09 +0100

    add bytecode files

---
+++ bytecodes/errors.m
@@ -0,0 +1,15 @@
+pall
+push 4
+push 10
+push 5
+push -2147483647
+push 6
+pall
+
+push	1
+pall asdlasdaijsadx
+ll push sad
+push 12356452.453
+push 1asd2354fsa
+push a
+push
---
+++ bytecodes/errors_3.m
@@ -0,0 +1,17 @@
+
+
+
+
+
+push 1
+push -1
+push 0
+push -0000010
+push 4
+pall
+push 5
+push -0
+push -100
+push 1000
+pall
+push --5
